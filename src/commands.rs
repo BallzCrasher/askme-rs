bae95b06fffc3b4847adea87e020f701115aba8b
BallzCrasher <ballzcrasher@gmail.com>
2022-09-27 23:56:56 +0300

    added anonymous funtionality

--- src/commands.rs
+++ src/commands.rs
@@ -25,13 +25,13 @@ pub fn get_questions(conn: &mut SqliteConnection) {
     }
 }
 
-pub fn ask_question(conn: &mut SqliteConnection, account: &Account) { 
+pub fn ask_question(conn: &mut SqliteConnection, account: &Account, anonymous: bool ) { 
     use crate::models::questions::dsl::*;
     let input = user_input::<String>("Question: ").unwrap();
     let qst = NewQuestion {
         content: input.as_str(),
         publisher: account.id,
-        is_anonymous: false
+        is_anonymous: anonymous
     };
 
     insert_into(questions)
@@ -47,7 +47,7 @@ pub fn get_users(conn: &mut SqliteConnection) {
         .expect("Error getting accounts");
 
     for account in results {
-        println!("{}", account.name);
+        println!("({}) -> {}", account.id, account.name);
     }
 }
 
@@ -79,7 +79,7 @@ pub fn open_thread(conn: &mut SqliteConnection, account: &Account) {
         }
     }
 
-    fn prompt_replay(conn: &mut SqliteConnection, thrd_id: i32, account_id: i32){ 
+    fn prompt_replay(conn: &mut SqliteConnection, thrd_id: i32, account_id: i32, anonymous: bool){ 
         use crate::models::answers::dsl::*;
         let input = user_input::<i32>("Enter the id of the post you wish to replay to (0 if replaying to the original post): ");
         if input.is_err() { println!("id must be a number"); return;}
@@ -98,8 +98,36 @@ pub fn open_thread(conn: &mut SqliteConnection, account: &Account) {
                 thread_id: thrd_id,
                 to_id: input_id,
                 publisher: account_id,
-                is_anonymous: false
+                is_anonymous: anonymous
             }).execute(conn).expect("Error connecting to database");
+    }
+
+    fn prompt_delete(conn: &mut SqliteConnection, thrd_id: i32, account_id: i32){ 
+        use crate::models::answers::dsl::*;
+        let input = user_input::<i32>("Enter the id of the post you wish to replay to (0 if replaying to the original post): ");
+        if input.is_err() { println!("id must be a number"); return;}
+        let input_id = input.unwrap();
+        // checking if there is post with such id
+        let result = answers
+            .filter(thread_id.eq(thrd_id))
+            .find(input_id)
+            .first::<Answer>(conn);
+        if  result.is_err() { 
+            println!("No Such Answer id belonging to this thread");
+            return;
+        }
+        // check if post belongs to the user
+        let user_answer = result.unwrap();
+        
+        if user_answer.publisher == account_id { 
+            diesel::delete(
+                answers
+                .filter(id.eq(user_answer.id))
+            ).execute(conn).expect("Failed to delete the post :: connection error");
+        } else { 
+            println!("Cannot delete posts which do not belong to you.");
+            return;
+        }
     }
 
     //get thread_id
@@ -122,20 +150,38 @@ pub fn open_thread(conn: &mut SqliteConnection, account: &Account) {
         if question.is_anonymous { 
             println!("(0) {} -- {}", question.content, "Anonymous" )
         } else { 
-            let publisher_account: Account = accounts
+            let publisher_account: Result<Account,_> = accounts
                 .find(question.publisher)
-                .first(conn).expect(format!("ERROR :: FOUND ID ({}) OF UNKNOWN USER",question.publisher).as_str());
-            println!("(0) {} -- {}", question.content, publisher_account.name );
+                .first(conn);
+            if publisher_account.is_err() { 
+                println!("(0) {} -- {}", question.content, "Deleted" );
+            } else { 
+                println!("(0) {} -- {}", question.content, publisher_account.unwrap().name );
+            }
         }
 
         show_thread(conn, thread_id, 0, 1);
-        println!("Commands: [ 0: back, 1: replay ]");
+        println!("Commands: [ 0: back, 1: replay, 2: anonymous replay, 3: delete ]");
         let input: String = user_input("-~> ").unwrap();
 
         match input.to_lowercase().trim() { 
             "0"|"back" => break,
-            "1"|"replay" => prompt_replay(conn,thread_id,account.id),
+            "1"|"replay" => prompt_replay(conn,thread_id,account.id, false),
+            "2"|"anonymous replay" => prompt_replay(conn,thread_id,account.id, true),
+            "3"|"delete" => prompt_delete(conn, thread_id, account.id),
             _ => continue
         }
+    }
+}
+
+pub fn user_answers(conn: &mut SqliteConnection, account: &Account){ 
+    use crate::models::answers::dsl::*;
+    let results: Vec<Answer> = answers
+        .filter(publisher.eq(account.id))
+        .load(conn)
+        .expect("Error connecting to database");
+
+    for answer in results { 
+        println!("({}) {} -> Thread: {}",answer.id, answer.content, answer.thread_id);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ use askme_diesel_rs::*;
 use commands::*;
 
 fn main() {
-
     let connection = &mut establish_connection("database.db");
     init_database(connection).expect("Error Initializing database");
 
@@ -10,14 +9,16 @@ fn main() {
     println!("Signed As: {}", user_account.name);
 
     loop {
-        println!("Commands: [ 1: questions, 2: ask, 3: open_thread, 4: users, 9: clear, 0: exit ]");
+        println!("Commands: [ 1: questions, 2: ask, 3: ask anonymous, 4: open thread, 5: users, 6: my answers, 9: clear, 0: exit ]");
         let input: String = user_input("Command: ").unwrap();
 
         match input.to_lowercase().trim() { 
             "1" | "questions" => get_questions(connection),
-            "2" | "ask" => ask_question(connection, &user_account) ,
-            "3" | "open_thread" => open_thread(connection, &user_account),
-            "4" | "users" => get_users(connection),
+            "2" | "ask" => ask_question(connection, &user_account, false),
+            "3" | "ask anonymous" => ask_question(connection, &user_account, true),
+            "4" | "open thread" => open_thread(connection, &user_account),
+            "5" | "users" => get_users(connection),
+            "6" | "my answers" => user_answers(connection, &user_account),
             "9" | "clear" => clear_screen(),
             "0" | "exit" => break,
             _ => continue
